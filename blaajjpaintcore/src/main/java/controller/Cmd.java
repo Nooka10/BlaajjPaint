462032fbd695e4c5fbb700a11e21ce1ead1dcdb6
Perceval <dnamella1@hotmail.com>
2018-03-18 18:48:41 +0100

    Record implemented as singleton

--- blaajjpaintcore/src/main/java/controller/Cmd.java
+++ blaajjpaintcore/src/main/java/controller/Cmd.java
@@ -1,35 +1,19 @@
 package controller;
 
+import model.RecordCmd;
 import utils.UndoException;
 
-public abstract class ICmd {
+public abstract class Cmd {
 
-    protected static long globalId = 0;
-
-    protected long id = -1;
-    protected long previousId = -1;
-
-    /**Applique les modifications au model */
-    public final void execute() {
-        previousId = globalId;
-        id = ++globalId;
+    /** Sauve la Cmd dans le RecordCmd*/
+    public final void save() {
+        RecordCmd.getInstance().SaveCmd(this);
     }
-
-    public abstract void
 
     /** Retourne le model à son état précédent l'exécution de la commande */
     public abstract void undo() throws UndoException;
 
     /**Retourne le model à son état suivant l'exécution de la commande */
     public abstract void redo() throws UndoException;
-
-    /** Retourne l'id de la commande si elle a été exécutée, sinon -1 */
-    public long getId() { return id; }
-
-    /** Retourne l'id de la commande précédent si celle-ci a été exécutée, sinon -1 */
-    public long getPreviousId() { return previousId; }
-
-    /** Retourne l'id global de la classe ICmd */
-    public static long getGlobalId() { return globalId; }
 }
 
